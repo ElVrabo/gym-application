c4953052a5a12cde899a134b715e9afea39ab44e
HectorAgHe <aguilarhector440@gmail.com>
2024-11-04 11:36:40 -0600

    cambio test pipeline-mocha

---
+++ test/test/example.test.js
@@ -0,0 +1,7 @@
+// test/example.test.js
+const assert = require('chai').assert; // O usa require('assert') si prefieres sin Chai
+describe('Test de ejemplo', () => {
+    it('should return true', () => {
+        assert.isTrue(true);
+    });
+});
